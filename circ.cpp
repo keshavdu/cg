1c1729997aab111333b8f7dca9deb0b1c2a8256f
keshavdu <104609530+keshavdu@users.noreply.github.com>
2022-04-29 08:52:31 +0530

    Add files via upload

---
+++ circ.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <graphics.h>
+using namespace std;
+int h=400,k=400,R=300;
+void plot(int x,int y)
+{
+	putpixel(h+x, k+y ,WHITE);
+	putpixel(h+y, k+x ,WHITE);
+	putpixel(h+y, k-x ,WHITE);
+	putpixel(h+x, k-y ,WHITE);
+	putpixel(h-x, k-y ,WHITE);
+	putpixel(h-y, k-x ,WHITE);
+	putpixel(h-y, k+x ,WHITE);
+	putpixel(h-x, k+y ,WHITE);
+	
+}
+void drawCirc(int h,int k,int R)
+{
+	int x,y,P;
+	x=0;
+	y=R;
+	P=1-R;
+	int gd=DETECT,gm;
+	initgraph(&gd, &gm, (char*)"");
+	//setbkcolor(WHITE);
+	plot(x, y);
+	while(x<=y)
+	{
+		if(P<0)
+		{
+			P+=2*x+3;
+		}
+		else 
+		{
+			P+=2*(x-y)+5;
+			y--;
+		}
+		x++;
+		plot(x, y);
+	}
+	getch();
+	closegraph();
+}
+int main()
+{
+	drawCirc(h,k,R);
+}
